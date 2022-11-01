f72d521fe9387f4b75fe714bbe6c21c499b69d6d
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2022-10-31 23:16:54 -0300

    Add handler for subscribe

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+// Extract form data using serde.
+// This handler gets called only if the request's content type is *x-www-form-urlencoded*
+// and content of the request can be deserialized to a `FormData` struct.
+async fn subscribe() -> impl Responder {
+    HttpResponse::Ok()
+}
+
 pub fn run(address: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
             App::new()
